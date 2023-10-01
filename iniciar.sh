d899e9fec5964d982415a15d508d8aab862cd5f8
Fernando <3001052740101@ingenieria.usac.edu.gt>
2023-09-30 23:03:09 -0600

    agregar sh

---
+++ iniciar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo insmod /home/archivos2s2022/so1_2s2023_201404106/proyecto1/modules/ram_201404106.ko
+
+sudo insmod /home/archivos2s2022/so1_2s2023_201404106/proyecto1/modules/cpu_201404106.ko
+
+sudo docker run --rm -it -p 4000:4000 backendgo:v1
