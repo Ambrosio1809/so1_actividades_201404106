9b5c9a9d1a09cd1c18aaf8a2aa2a9b33e9e619a5
Fernando <3001052740101@ingenieria.usac.edu.gt>
2023-10-01 00:17:40 -0600

    editar

--- iniciar.sh
+++ iniciar.sh
@@ -4,4 +4,4 @@ sudo insmod /home/archivos2s2022/so1_2s2023_201404106/proyecto1/modules/ram_2014
 
 sudo insmod /home/archivos2s2022/so1_2s2023_201404106/proyecto1/modules/cpu_201404106.ko
 
-sudo docker run --rm -it -p 4000:4000 backendgo:v1
+sudo docker run -p 4000:4000 backendgo:v1
